45047d37df346c25609ace998db4869df061b06f
Nilo <nilo_2804@hotmail.com>
2026-05-22 22:45:45 -0500

    creaacion de metodos crud, tienda y prueba inicial

--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -1,11 +1,32 @@
 //https://styled-components.com/
 import styled from "styled-components";
 import { CategoriasTemplate } from "../components/templates/CategoriasTemplate";
+import { useCategoriasStore } from "../store/CategoriasStore";
+import { useQuery } from "@tanstack/react-query";
+import { useUsuariosStore } from "../store/UsuariosStore";
 
 export function Categorias() {
+  const { datacategoria, mostrarCategorias } = useCategoriasStore();
+  const { datausuarios } = useUsuariosStore();
+  // CORRECCIÓN: Sintaxis de objeto para useQuery
+  const { isLoading, error } = useQuery({
+    queryKey: ["mostrar categorias", datausuarios?.id],
+    queryFn: () => mostrarCategorias({ idusuario: datausuarios.id, tipo: "i" }),
+    enabled: !!datausuarios?.id, // Solo se ejecuta si el usuario ya cargó
+  });
+  if (isLoading) {
+    return <h1>Cargando...</h1>;
+  }
+  if (error) {
+    return <h1>Error...</h1>;
+  }
+
   return (
     <>
       <Container>
+        {datacategoria.map((item, index) => {
+          return <h1>{item.descripcion}</h1>;
+        })}
         <CategoriasTemplate />
       </Container>
     </>
---
+++ src/store/CategoriasStore.jsx
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import {
+  EditarCategorias,
+  EliminarCategorias,
+  InsertarCategorias,
+  MostrarCategorias,
+} from "../supabase/crudCategorias";
+
+export const useCategoriasStore = create((set, get) => ({
+  datacategoria: [],
+
+  mostrarCategorias: async (p) => {
+    const response = await MostrarCategorias(p);
+    set({ datacategoria: response });
+    return response;
+  },
+
+  insertarCategorias: async (p) => {
+    await InsertarCategorias(p);
+    const { mostrarCategorias } = get();
+    set(mostrarCategorias(p));
+  },
+
+  eliminarCategorias: async (p) => {
+    await EliminarCategorias(p);
+    const { mostrarCategorias } = get();
+    set(mostrarCategorias(p));
+  },
+  editarCategorias: async (p) => {
+    await EditarCategorias(p);
+    const { mostrarCategorias } = get();
+    set(mostrarCategorias(p));
+  },
+}));
